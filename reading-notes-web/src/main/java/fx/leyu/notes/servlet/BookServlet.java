9723dbd654c95e47d514b05873e1e8e6e5492f72
fxleyu <fxleyu@qq.com>
2019-01-23 11:12:00 +0800

    [0.0.1] 添加 book 接口，用于存/取数据

---
+++ reading-notes-web/src/main/java/fx/leyu/notes/servlet/BookServlet.java
@@ -0,0 +1,36 @@
+package fx.leyu.notes.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class BookServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        write(resp, "Json String");
+    }
+
+    private void write(HttpServletResponse resp, String json) {
+        PrintWriter writer = null;
+        try {
+            resp.setCharacterEncoding("utf-8");
+            writer = resp.getWriter();
+            writer.write(json);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
+}
