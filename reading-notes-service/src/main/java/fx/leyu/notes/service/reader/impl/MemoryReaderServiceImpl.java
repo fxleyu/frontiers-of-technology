804b097631edce2db533dfb8deda47f4ee9912d6
fxleyu <fxleyu@qq.com>
2019-02-02 22:38:38 +0800

    [0.5.4] 实现 Reader 信息更新服务

--- reading-notes-service/src/main/java/fx/leyu/notes/service/reader/impl/MemoryReaderServiceImpl.java
+++ reading-notes-service/src/main/java/fx/leyu/notes/service/reader/impl/MemoryReaderServiceImpl.java
@@ -3,7 +3,6 @@ package fx.leyu.notes.service.reader.impl;
 import com.google.common.collect.Maps;
 import fx.leyu.notes.domain.Reader;
 import fx.leyu.notes.service.reader.ReaderService;
-import org.apache.commons.lang3.StringUtils;
 
 import java.util.Map;
 
@@ -12,24 +11,27 @@ public class MemoryReaderServiceImpl implements ReaderService {
 
     @Override
     public void updateName(long readerId, String name) {
-        if (StringUtils.isBlank(name)) {
-            throw new IllegalArgumentException("name MUST BE NOT blank");
-        }
+        Reader reader = gainReader(readerId);
+        reader.setName(name);
+    }
 
+    private Reader gainReader(long readerId) {
         Reader reader = STORAGE.get(readerId);
         if (reader == null) {
             throw new IllegalArgumentException("the reader is illegal");
         }
-        reader.setName(name);
+        return reader;
     }
 
     @Override
     public void updateEmail(long readerId, String email) {
-
+        Reader reader = gainReader(readerId);
+        reader.setEmail(email);
     }
 
     @Override
     public void updateLocation(long readerId, String location) {
-
+        Reader reader = gainReader(readerId);
+        reader.setLocation(location);
     }
 }
